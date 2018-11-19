ef24e8867038e1634712c579cce8c66ffa30df3c
kinoshita <kinoshita@i-system.co.jp>
2018-11-19 14:53:21 +0900

    いろいろおわった

--- routes/done.js
+++ routes/done.js
@@ -1,34 +1,46 @@
-var express = require('express');
-var async = require('async');
+var express = require("express");
+var async = require("async");
 var router = express.Router();
-var execQuery = require('./db.js');
+var execQuery = require("./db.js");
 
-router.post('/', async function(req, res, next) {
+router.post("/", async function(req, res, next) {
+  console.log("=============================");
   console.log("done post");
   console.log(req.body);
 
-  var query = "insert into dones(year, month, day, work_id) values ('"
-                   + req.body.year + "', '"
-                   + req.body.month + "', '"
-                   + req.body.day + "', '"
-                   + req.body.work_id + "')";
+  var query =
+    "insert into dones(year, month, day, work_id) values ('" +
+    req.body.year +
+    "', '" +
+    req.body.month +
+    "', '" +
+    req.body.day +
+    "', '" +
+    req.body.work_id +
+    "')";
   await execQuery(query);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.delete('/', async function(req, res, next) {
+router.delete("/", async function(req, res, next) {
+  console.log("=============================");
   console.log("done delete");
   console.log(req.body);
 
-  var query = "delete dones where year='"
-                   + req.body.year + "' and month='"
-                   + req.body.month + "' and day='"
-                   + req.body.day + "' and work_id='"
-                   + req.body.work_id + "'";
+  var query =
+    "delete dones where year='" +
+    req.body.year +
+    "' and month='" +
+    req.body.month +
+    "' and day='" +
+    req.body.day +
+    "' and work_id='" +
+    req.body.work_id +
+    "'";
   await execQuery(query);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
--- routes/work.js
+++ routes/work.js
@@ -1,29 +1,31 @@
-var express = require('express');
-var async = require('async');
+var express = require("express");
+var async = require("async");
 var router = express.Router();
-var execQuery = require('./db.js');
+var execQuery = require("./db.js");
 
-router.post('/', async function(req, res, next) {
+router.post("/", async function(req, res, next) {
+  console.log("=============================");
   console.log("work post");
   console.log(req.body);
 
-  var queryWorks = "insert into works(work) values ('"+req.body.work+"')";
+  var queryWorks = "insert into works(work) values ('" + req.body.work + "')";
   await execQuery(queryWorks);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.delete('/', async function(req, res, next) {
+router.delete("/", async function(req, res, next) {
+  console.log("=============================");
   console.log("work delete");
   console.log(req.body);
 
-  var queryWorks = "delete works where work_id='"+req.body.work_id+"'";
-  var queryDones = "delete dones where work_id='"+req.body.work_id+"'";
-  
+  var queryWorks = "delete works where work_id='" + req.body.work_id + "'";
+  var queryDones = "delete dones where work_id='" + req.body.work_id + "'";
+
   await execQuery(queryWorks);
   await execQuery(queryDones);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
